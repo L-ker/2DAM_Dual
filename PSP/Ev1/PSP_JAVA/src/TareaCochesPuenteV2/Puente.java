a89cf407cfec258feb1159f44d8f6eac4f2fe618
Luker <lucasperezgar@gmail.com>
2025-11-11 20:24:17 +0100

    si

--- PSP/Ev1/PSP_JAVA/src/TareaCochesPuenteV2/Puente.java
+++ PSP/Ev1/PSP_JAVA/src/TareaCochesPuenteV2/Puente.java
@@ -8,16 +8,40 @@ public class Puente {
     private int cochesEnPuente = 0;
     private int pesoActual = 0;
 
-    public Puente(LinkedBlockingQueue<Coche> cola) {
-        this.cola = cola;
+    public Puente() {
+    	cola = new LinkedBlockingQueue<Coche>();
     }
 
+    /*
+     * Sobre esto tengo una duda que quiero preguntarte en clase porque por lo que he visto esto funciona
+     * pero se me hace "raro" que los coches deban gestionar otros coches en vez de a si mismos y quiero
+     * saber si hay una mejor forma de hacerlo o una forma que parezca "tener mas sentido"
+     */
     public void entrar(Coche coche) {
-    	
-    
+        try {
+        	cola.put(coche);
+        	Coche c = cola.take();
+			
+        	synchronized (this) {
+
+                while (cochesEnPuente + 1 > MAX_COCHES || pesoActual + c.getPeso() > MAX_PESO) {
+                    wait();
+                }
+                cochesEnPuente++;
+                pesoActual += coche.getPeso();
+                System.out.println("Coche " + coche.getCategoria() + " entrando al puente. Coches en puente: " + cochesEnPuente + ", peso: " + pesoActual);
+        	}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
     }
     
     public void salir(Coche coche) {
-    	
+        synchronized (this) {
+            cochesEnPuente--;
+            pesoActual -= coche.getPeso();
+            System.out.println("Coche " + coche.getCategoria() + " ha salido del puente. Coches en puente: " + cochesEnPuente + ", peso: " + pesoActual);
+            notifyAll();
+        }
     }
 }
